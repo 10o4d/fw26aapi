4f41f14672f94d2994fc7a8d37e517a187d9aef5
10o4d <149402618+10o4d@users.noreply.github.com>
2025-12-05 11:28:51 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import express from 'express'
+const express = require('express');
+const bodyParser = require("body-parser");
+const cors = require('cors');
+const app = express();
+const port = 3000
 
-const app = express()
+app.set("view engine", "ejs")
+app.set("views", "./views")
 
-app.get('/', (_req, res) => {
-  res.send('Hello Express!')
-})
+app.use(bodyParser.json());
+/* estas linhas são para leitura de de body.json */
+app.use(express.json());
+app.use(express.urlencoded());
+/* aqui é o acesso exerno do sistema para a api */
 
-app.get('/api/users/:id', (_req, res) => {
-  res.json({ id: _req.params.id })
-})
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
-app.get('/api/posts/:postId/comments/:commentId', (_req, res) => {
-  res.json({ postId: _req.params.postId, commentId: _req.params.commentId })
-})
+//rotas
+require('../db/dbconnect')(app);
+require('../route/home')(app);
+require('../route/getnoticias')(app);
+require('../route/getidnoticias')(app);
+require('../route/gettiponoticias')(app);
+require('../route/delnoticias')(app);
+require('../route/postnoticias')(app);
+require('../route/putnoticias')(app);
 
-export default app
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
